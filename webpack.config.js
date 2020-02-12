ea0fac05d7ff6f9bea107e96c266feef97748018
Andrew Mayer <andrewmayer515@gmail.com>
2020-02-12 09:20:07 -0600

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,50 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/client/public/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
+module.exports = env => {
+  const devtool = env.NODE_ENV !== 'production' ? 'inline-source-map' : '';
+  return {
+    entry: './src/client/public/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
           },
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/client/public/index.html',
-      filename: './index.html',
-    }),
-  ],
-  devServer: {
-    proxy: {
-      '/api': 'http://localhost:5000',
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+            },
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
+      ],
     },
-  },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './src/client/public/index.html',
+        filename: './index.html',
+      }),
+    ],
+    devServer: {
+      proxy: {
+        '/api': 'http://localhost:5000',
+      },
+    },
+    devtool,
+  };
 };
