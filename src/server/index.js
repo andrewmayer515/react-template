182c48203bd53c67a8853df760f2b4c78e1ead38
Andrew Mayer <andrewmayer515@gmail.com>
2020-02-11 19:18:14 -0600

    dotenv

--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,18 @@
 import express from 'express';
 import path from 'path';
+import dotenv from 'dotenv';
 import example from './routes/example';
 
+dotenv.config();
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../../dist')));
+if (process.env.NODE_ENV !== 'development') {
+  app.use(express.static(path.join(__dirname, '../dist')));
+}
 
 // Routes
 app.use(example);
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../dist/index.html'));
-});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
